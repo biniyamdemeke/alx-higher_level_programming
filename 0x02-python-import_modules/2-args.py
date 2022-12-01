bab74a109b4a5b85d9b70cd964b5106f104f2049
biniyamdemeke <biniyamdemeke7@gmail.com>
2022-12-01 06:57:32 -0800

    Task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if args == 1:
         print("{:d} argument:".format(args))
         print("{:d}: {}".format(args, sys.argv[1]))
-    if args == 0:
-        print("{:d} arguments.".format(args))
-    if args >= 2:
-        print("{:d} arguments:".format(args))
-        for i in range(1, (len(sys.argv))):
-            print("{:d}: {}".format(i, sys.argv[i]))
+        if args == 0:
+            print("{:d} arguments.".format(args))
+            if args >= 2:
+                print("{:d} arguments:".format(args))
+                for i in range(1, (len(sys.argv))):
+                    print("{:d}: {}".format(i, sys.argv[i]))
